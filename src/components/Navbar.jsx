d699d7373f16f9b51ce6d22c86b254592da78cb4
Your Name <you@example.com>
2024-03-30 12:01:05 +0600

    change some the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -7,7 +6,13 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between pt-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="web logo" />
+        {/* <img className="mx-2 w-10" src={logo} alt="web logo" /> */}
+        <a
+          className="font-semibold text-xl"
+          href="https://github.com/xdevimran"
+        >
+          <span className="text-2xl">X</span>imra
+        </a>
       </div>
       <div className="flex m-8 items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
